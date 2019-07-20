2dcca290f038b9088181c6d29ef114010be47183
gkotecki <gabrielkotecki@gmail.com>
2019-07-19 21:21:08 -0300

    first commit

---
+++ xamarin-up-adv/xamarin-up-adv/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using xamarin_up_adv.Services;
+using xamarin_up_adv.Views;
+
+namespace xamarin_up_adv
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ xamarin-up-adv/xamarin-up-adv/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using xamarin_up_adv.Models;
+
+namespace xamarin_up_adv.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
