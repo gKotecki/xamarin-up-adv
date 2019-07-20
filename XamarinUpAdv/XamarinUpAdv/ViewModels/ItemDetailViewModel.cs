16e7e581eadaa331d9e4d4717212f78b6387ebe7
gkotecki <gabrielkotecki@gmail.com>
2019-07-19 21:43:15 -0300

    correcao build

---
+++ XamarinUpAdv/XamarinUpAdv/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinUpAdv.Models;
+
+namespace XamarinUpAdv.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
